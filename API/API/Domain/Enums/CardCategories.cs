1734933f8df5fa2e4078ac6e063db45e8f3418f0
Filip Kmiec <kmiec.filip@gmail.com>
2026-06-20 00:00:55 +0200

    added RecommendationTypes and CardCategories enums

---
+++ API/API/Domain/Enums/CardCategories.cs
@@ -0,0 +1,15 @@
+namespace API.Domain.Enums
+{
+    /// <summary>
+    /// Category of a card used in UI representations (albums, playlists, tracks).
+    /// </summary>
+    public enum CardCategories
+    {
+        Album = 1,
+        Playlist = 2,
+        Track = 3,
+        Podcast = 4,
+        Soundtrack = 5,
+        Creator = 6
+    }
+}
---
+++ API/API/Domain/Enums/RecommendationTypes.cs
@@ -0,0 +1,11 @@
+﻿namespace API.Domain.Enums
+{
+    public enum RecommendationTypes
+    {
+        MostViewed,
+        MostViewedByUser,
+        MostNoted,
+        MostNotedByUser,
+        FollowsByUser
+    }
+}
